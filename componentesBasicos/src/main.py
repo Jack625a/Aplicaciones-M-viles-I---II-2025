f316c60d594a9f36ca4f7eca3a07e2a9b2a20d93
Jack625a <ktfut15@gmail.com>
2025-08-22 14:32:07 -0400

    Buttones

--- componentesBasicos/src/main.py
+++ componentesBasicos/src/main.py
@@ -1,18 +1,37 @@
 #IMPORTACION
 import flet as ft
 
+
+    
+
 #FUNCION PRINCIPAL
 def main(page: ft.Page):
-    
+    #fUNCION ALERTA
+    alerta=ft.AlertDialog(
+        title=ft.Text("Alerta"),
+        content=ft.Text("Mensaje de Prueba"),
+        actions=[ft.CupertinoButton(text="Salir")],
+        #modal=True
+        )
+       
+
+
+
+    #Variable de control
+    page.floating_action_button=(ft.FloatingActionButton(icon=ft.icons.HOME,bgcolor=ft.colors.PURPLE,on_click=lambda e:page.open(alerta)))
     #BOTONES
-    boton=ft.Button(text="Boton 1",bgcolor=ft.colors.BLUE_50,icon=ft.icons.HOME,)
+    boton=ft.Button(text="Boton 1",bgcolor=ft.colors.BLUE_50,icon=ft.icons.HOME,on_click=lambda e:page.open(alerta) )
     boton2=ft.CupertinoFilledButton(text="Boton 2",
                                     icon=ft.icons.FACE,
                                     icon_color=ft.colors.CYAN_ACCENT_200)
     boton3=ft.FilledButton(text="Boton 3",icon=ft.icons.TIKTOK)
+
+    boton4=ft.IconButton(icon=ft.icons.IMAGE)
+    
+    pruebaBoton=ft.FloatingActionButton(icon=ft.icons.HOME)
     #Componentes de la interfaz
     page.add(
-        boton,boton2,boton3   
+        boton,boton2,boton3,boton4, pruebaBoton 
     )
 
 
